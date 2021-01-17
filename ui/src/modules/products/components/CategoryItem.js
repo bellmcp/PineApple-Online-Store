11a0abb814b1f05cf460b24f90087bdfdb450ebc
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-01-17 22:33:13 +0700

    Query string with useLocation

--- ui/src/modules/products/components/CategoryItem.js
+++ ui/src/modules/products/components/CategoryItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useHistory, useRouteMatch } from 'react-router-dom'
 import { Grid, Chip } from '@material-ui/core'
 
 export default function CategoryItem({ title, Icon }) {
+  const history = useHistory()
+  const { path } = useRouteMatch()
+
+  const filterProductsByCategory = () =>
+    history.push(`${path}?category=${title}`) //QUERY STRING
+
   return (
-    <Grid item>
+    <Grid item onClick={filterProductsByCategory}>
       <Chip icon={<Icon />} label={title} clickable color="primary"></Chip>
     </Grid>
   )
--- ui/src/modules/products/components/ProductList.js
+++ ui/src/modules/products/components/ProductList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import axios from 'axios'
+import queryString from 'query-string'
 import { Typography, Grid, CircularProgress } from '@material-ui/core/'
 import { makeStyles } from '@material-ui/core/styles'
 import CategoryList from './CategoryList'
@@ -17,25 +19,27 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ProductList() {
   const classes = useStyles()
+  const { search } = useLocation()
+  const { category } = queryString.parse(search) //Return Key (Category) and Value
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const loadProducts = async () => {
       setIsLoading(true)
-      const { data } = await axios.get('/products')
+      const { data } = await axios.get(`/products${search}`) //FETCH ONLY MATCHED
 
       setProducts(data)
       setIsLoading(false)
     }
 
     loadProducts()
-  }, [])
+  }, [search])
 
   return (
     <div>
       <Typography variant="h4" component="h1" className={classes.title}>
-        All Products
+        {category || 'All'} Products
       </Typography>
       <CategoryList></CategoryList>
       {isLoading ? (
