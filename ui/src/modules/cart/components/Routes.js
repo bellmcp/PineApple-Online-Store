f6779ee95197544a758014ba026d76a317198395
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-01-17 21:10:12 +0700

    useRouteMatch

--- ui/src/modules/cart/components/Routes.js
+++ ui/src/modules/cart/components/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import Cart from './Cart'
 
 export default function Routes() {
+  const { path } = useRouteMatch()
+
   return (
     <Switch>
-      <Route path="/cart">
+      <Route path={path}>
         <Cart></Cart>
       </Route>
     </Switch>
--- ui/src/modules/products/components/Routes.js
+++ ui/src/modules/products/components/Routes.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import ProductList from './ProductList'
 import ProductDetails from './ProductDetails'
 
 export default function Routes() {
+  const { path } = useRouteMatch()
+
   return (
     <Switch>
-      <Route path="/products/:id">
+      <Route path={`${path}/:id`}>
         <ProductDetails></ProductDetails>
       </Route>
-      <Route path="/products">
+      <Route path={path}>
         <ProductList></ProductList>
       </Route>
     </Switch>
