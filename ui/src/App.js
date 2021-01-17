525abe216aeec77eb2583f37f6d7e60a3796fc19
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-01-18 00:10:50 +0700

    Form validation

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,48 +1,48 @@
-// import React from 'react'
-// import { BrowserRouter as Router } from 'react-router-dom'
-// import Layout from 'modules/ui/components/Layout'
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <Layout></Layout>
-//     </Router>
-//   )
-// }
-
 import React from 'react'
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Layout from 'modules/ui/components/Layout'
 
 export default function App() {
-  const { register, handleSubmit, errors } = useForm({
-    mode: 'onBlur',
-    defaultValues: {
-      gender: 'Male',
-    },
-    resolver: yupResolver(
-      yup.object().shape({
-        email: yup.string().required(),
-        password: yup.string().min(8).required(),
-        gender: yup.mixed().oneOf(['Male', 'Female']),
-      })
-    ),
-  })
-
-  const submit = (value) => {
-    console.log(value)
-  }
-
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      <input type="text" name="email" ref={register} />
-      {errors.email && <div>{errors.email.message}</div>}
-      <input type="password" name="password" ref={register} />
-      {errors.password && <div>{errors.password.message}</div>}
-      <input type="text" name="gender" ref={register} />
-      {errors.gender && <div>{errors.gender.message}</div>}
-      <button type="submit">Submit</button>
-    </form>
+    <Router>
+      <Layout></Layout>
+    </Router>
   )
 }
+
+// import React from 'react'
+// import { useForm } from 'react-hook-form'
+// import * as yup from 'yup'
+// import { yupResolver } from '@hookform/resolvers/yup'
+
+// export default function App() {
+//   const { register, handleSubmit, errors } = useForm({
+//     mode: 'onBlur',
+//     defaultValues: {
+//       gender: 'Male',
+//     },
+//     resolver: yupResolver(
+//       yup.object().shape({
+//         email: yup.string().required(),
+//         password: yup.string().min(8).required(),
+//         gender: yup.mixed().oneOf(['Male', 'Female']),
+//       })
+//     ),
+//   })
+
+//   const submit = (value) => {
+//     console.log(value)
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit(submit)}>
+//       <input type="text" name="email" ref={register} />
+//       {errors.email && <div>{errors.email.message}</div>}
+//       <input type="password" name="password" ref={register} />
+//       {errors.password && <div>{errors.password.message}</div>}
+//       <input type="text" name="gender" ref={register} />
+//       {errors.gender && <div>{errors.gender.message}</div>}
+//       <button type="submit">Submit</button>
+//     </form>
+//   )
+// }
--- ui/src/modules/cart/components/Delivery.js
+++ ui/src/modules/cart/components/Delivery.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import {
@@ -22,30 +25,53 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Delivery({ onSubmit }) {
   const classes = useStyles()
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(
+      yup.object().shape({
+        name: yup.string().required(),
+        email: yup.string().email().required(),
+        address: yup.string().required(),
+      })
+    ),
+  })
+
+  console.log(errors)
+
+  const submit = (deliveryInfo) => {
+    console.log(deliveryInfo)
+  }
 
   return (
-    <form autoComplete="off">
+    <form onSubmit={handleSubmit(submit)} autoComplete="off">
       <Card>
         <CardContent className={classes.form}>
           <Typography variant="h5" component="h2">
             Delivery Information
           </Typography>
           <TextField
+            inputRef={register}
             variant="outlined"
             label="Name"
             placeholder="Enter your fullname"
             name="name"
             fullWidth
+            helperText={errors.name?.message || ''}
+            error={!!errors.name}
           />
           <TextField
+            inputRef={register}
             type="email"
             variant="outlined"
             label="Email"
             placeholder="Enter your email"
             name="email"
             fullWidth
+            helperText={errors.email?.message || ''}
+            error={!!errors.email}
           />
           <TextField
+            inputRef={register}
             multiline
             rows={4}
             variant="outlined"
@@ -53,6 +79,8 @@ export default function Delivery({ onSubmit }) {
             placeholder="Enter your address"
             name="address"
             fullWidth
+            helperText={errors.address?.message || ''}
+            error={!!errors.address}
           />
         </CardContent>
         <CardActions>
