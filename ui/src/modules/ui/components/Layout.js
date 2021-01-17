366098b61d3ca477c3d09b2ed774d71e9449b9e9
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-01-17 15:22:13 +0700

    Implement darkmode

--- ui/src/modules/ui/components/Layout.js
+++ ui/src/modules/ui/components/Layout.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, useMediaQuery } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import Header from './Header'
 import Content from './Content'
 
 export default function Layout() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = createMuiTheme({
+    palette: {
+      type: prefersDarkMode ? 'dark' : 'light',
+      primary: {
+        main: '#1abc9c',
+      },
+    },
+  })
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline></CssBaseline>
       <Header></Header>
       <Content></Content>
-    </>
+    </ThemeProvider>
   )
 }
