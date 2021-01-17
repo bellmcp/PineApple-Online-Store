a30c1b8c224cc44e96ba13a669962565c833df09
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-01-17 22:53:14 +0700

    Redirect

--- ui/src/modules/ui/components/Routes.js
+++ ui/src/modules/ui/components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import ProductRoutes from 'modules/products/components/Routes'
 import CartRoutes from 'modules/cart/components/Routes'
 
@@ -11,6 +11,12 @@ export default function Routes() {
       </Route>
       <Route path="/cart">
         <CartRoutes></CartRoutes>
+      </Route>
+      <Route exact path="/">
+        <Redirect to="/products"></Redirect>
+      </Route>
+      <Route>
+        <h1 style={{ textAlign: 'center' }}>Page not found</h1>
       </Route>
     </Switch>
   )
