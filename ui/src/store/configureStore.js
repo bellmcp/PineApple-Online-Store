4dcb8933d137ec02381cb0c3c410b92fef55769b
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-02-12 16:31:24 +0700

    Temporary expose redux store

--- ui/src/store/configureStore.js
+++ ui/src/store/configureStore.js
@@ -1,7 +1,7 @@
 switch (process.env.NODE_ENV) {
-  case 'production':
-    module.exports = require('./configureStore.prod')
-    break
+  // case 'production':
+  //   module.exports = require('./configureStore.prod')
+  //   break
   default:
     module.exports = require('./configureStore.dev')
 }
